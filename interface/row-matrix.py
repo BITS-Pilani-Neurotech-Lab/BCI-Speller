0d85dc74c672bc2e3acea27ec3e35ba4d53bb25d
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2025-01-08 11:38:04 +0530

    row-column matrix interface cpmpleted

--- interface/row-matrix.py
+++ interface/row-matrix.py
@@ -4,6 +4,7 @@ from random import randrange
 
 root = tk.Tk()
 root.title("P300 Row-matrix speller")
+root.configure(bg = 'black')
 # root.geometry('600x600')
 
 matrix = [
@@ -18,14 +19,23 @@ matrix = [
 ROWS = len(matrix)
 COLS = len(matrix[0])
 
+FLASH_INTERVAL = 500
+
 curr_row = None
 curr_col = None
+
+selected_row = None
+selected_col = None
 
 labels = [[None] * COLS for row in range(ROWS)]
 for row in range(ROWS):
 	for col in range(COLS):
-		labels[row][col] = tk.Label(root,text = matrix[row][col], bg = 'black', fg = 'white', font = ('Arial', 20))
+		labels[row][col] = tk.Label(root, text = matrix[row][col], bg = 'black', fg = 'white', font = ('Arial', 20))
 		labels[row][col].grid(row = row, column = col, sticky = tk.NSEW, ipadx = 50, ipady = 50)
+
+text_label = tk.Label(root, bg = 'black', fg = 'white', font = ('Arial', 50))
+text_label.grid(row = ROWS, column = 0, columnspan = COLS, ipadx = 50, ipady = 50)
+text = ''
 
 def hightlight_row(row):
 	global curr_row
@@ -47,13 +57,39 @@ def hightlight_col(col):
 			labels[row][col].configure(fg = 'red')
 	curr_col = col
 
-def flip():
-	if curr_row == 0:
+def event_detected(event):
+	global selected_row, selected_col
+	if curr_row is not None:
+		selected_row = curr_row
+	if curr_col is not None:
+		selected_col = curr_col
+
+def add_char():
+	global text, selected_row, selected_col
+	text += matrix[selected_row][selected_col]
+	text_label.configure(text = text)
+	selected_row = None
+	selected_col = None
+
+def select_row():
+	if selected_row is not None:
 		hightlight_row(None)
-	else:
-		hightlight_row(randrange(0, ROWS))
-	root.after(1000, flip)
+		root.after(FLASH_INTERVAL, select_col)
+		return
+	row = randrange(ROWS)
+	hightlight_row(row)
+	root.after(FLASH_INTERVAL, select_row)
 
-flip()
+def select_col():
+	if selected_col is not None:
+		add_char()
+		hightlight_col(None)
+		root.after(FLASH_INTERVAL, select_row)
+		return
+	col = randrange(ROWS)
+	hightlight_col(col)
+	root.after(FLASH_INTERVAL, select_col)
 
+root.bind('<space>', event_detected)
+select_row()
 root.mainloop()
